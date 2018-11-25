b27dd6ba3a52006125052c1a70759a4022de2853
kyrene <sjtv5.12@gmail.com>
2018-11-25 23:05:24 +0800

    stram 的基本用法

--- src/main/java/com/github/kyrenesjtv/day6/SimpleStream.java
+++ src/main/java/com/github/kyrenesjtv/day6/SimpleStream.java
@@ -1,6 +1,9 @@
 package com.github.kyrenesjtv.day6;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.*;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -12,44 +15,73 @@ import static java.util.stream.Collectors.toList;
 public class SimpleStream {
 
 
-    private static List<String> getDishNamesByCollections(List<Dish> menu){
+    private static List<String> getDishNamesByCollections(List<Dish> menu) {
         List<Dish> lowColories = new ArrayList<>();
-        for(Dish d :menu){
-            if(d.getCalories()<400){
+        for (Dish d : menu) {
+            if (d.getCalories() < 400) {
                 lowColories.add(d);
             }
         }
-        Collections.sort(lowColories,(o1,o2)->Integer.compare(o1.getCalories(),o2.getCalories()));
+        Collections.sort(lowColories, (o1, o2) -> Integer.compare(o1.getCalories(), o2.getCalories()));
 
         List<String> dishNamesList = new ArrayList<>();
-        for(Dish d :lowColories){
+        for (Dish d : lowColories) {
             dishNamesList.add(d.getName());
         }
         return dishNamesList;
     }
 
-    private static List<String> getDishNamesByStram(List<Dish> menus){
-       return menus.stream().filter((d) -> d.getCalories()<400)
-                .sorted(Comparator.comparing(Dish ::getCalories)).map(Dish::getName).collect(toList());
+    private static List<String> getDishNamesByStram(List<Dish> menus) {
+        return menus.stream()
+                .filter((d) -> d.getCalories() < 400)
+                .sorted(Comparator.comparing(Dish::getCalories)).map(Dish::getName).collect(toList());
     }
 
     public static void main(String[] args) {
-        List<Dish>  menu = Arrays.asList(
-                new Dish("pork",false,800,Dish.Type.MEAT),
-                new Dish("beef",false,700,Dish.Type.MEAT),
-                new Dish("chicken",false,400,Dish.Type.MEAT),
-                new Dish("french",true,530,Dish.Type.OTHER),
-                new Dish("rice",true,350,Dish.Type.OTHER),
-                new Dish("season fruit",true,120,Dish.Type.OTHER),
-                new Dish("pizza",true,550,Dish.Type.OTHER),
-                new Dish("prawns",false,300,Dish.Type.FISH),
-                new Dish("salmon",false,450,Dish.Type.FISH)
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT)
+                , new Dish("beef", false, 700, Dish.Type.MEAT)
+                , new Dish("chicken", false, 400, Dish.Type.MEAT)
+                , new Dish("french", true, 530, Dish.Type.OTHER)
+                , new Dish("rice", true, 350, Dish.Type.OTHER)
+                , new Dish("season fruit", true, 120, Dish.Type.OTHER)
+                , new Dish("pizza", true, 550, Dish.Type.OTHER)
+                , new Dish("prawns", false, 300, Dish.Type.FISH)
+                , new Dish("salmon", false, 450, Dish.Type.FISH)
         );
 
         List<String> dishNamesByCollections = getDishNamesByCollections(menu);
         System.out.println(dishNamesByCollections);
         List<String> dishNamesByStram = getDishNamesByStram(menu);
         System.out.println(dishNamesByStram);
+        /*******************************/
+        //        menu.stream().forEach(System.out::println);
+        menu.stream().forEach((a) -> System.out.println(a));
+
+        /*******************************/
+
+        List<String> collect = menu.stream().filter((d) -> {
+            System.out.println("filtering->" + d.getName());
+            return d.getCalories() > 300;
+        }).map(d -> {
+            System.out.println("filtering->" + d.getName());
+            return d.getName();
+        }).limit(3).collect(toList());
+
+        System.out.println(collect);
+
+        /*******************************/
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
